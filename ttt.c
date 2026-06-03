471bfb8617e8688c441a97b257ccd70d033b1f14
R. Farris <casualenglishmajour@gmail.com>
2026-06-03 00:50:04 -0600

    0.0.3 --- Basic window with text

--- ttt.c
+++ ttt.c
@@ -1,10 +1,26 @@
 #include "include/raylib.h"
 #include <stdio.h>
 
+const int screenWidth = 900;
+const int screenHeight = 600;
+const int fontSize = 32;
+const int fps = 30;
+
 int main(void)
 {
-	printf("Hello World!");
+	InitWindow(screenWidth, screenHeight, "Initial window");
 	
+	SetTargetFPS(fps);
+	
+	while(!WindowShouldClose()){
+		BeginDrawing();
+			ClearBackground(BLACK);
+			
+			DrawText("Good job!", screenWidth/2 - MeasureText("Good job!", fontSize)/2, screenHeight/2 - fontSize/2, fontSize, GREEN);
+		EndDrawing();
+	}
+	
+	CloseWindow();
 	
 	return 0;
 }
